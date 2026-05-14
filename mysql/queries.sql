02f2cbc4b9d512741dbd91b8795d8a7a64da792a
birishh <birish.liku.w@gmail.com>
2026-05-14 17:32:20 +0300

    Update queries.sql
    Added values to the  instructor table

--- mysql/queries.sql
+++ mysql/queries.sql
@@ -3,3 +3,9 @@ INSERT INTO Department VALUES
 (2, 'Software Engineering'),
 (3, 'environmental enginnering'),
 (4, 'electrical enginnering');
+
+INSERT INTO Instructor VALUES
+(201, 'Bekele', 'Alemu', 'bekele@aastu.edu.et', 1),
+(202, 'Hana', 'Tesfaye', 'hana@aastu.edu.et', 2),
+(203, 'Samuel', 'Taye', 'samuel@aastu.edu.et', 3),
+(204, 'Rahel', 'Kebede', 'rahel@aastu.edu.et', 4);
