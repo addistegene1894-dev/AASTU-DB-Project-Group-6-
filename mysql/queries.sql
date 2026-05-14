971c2b23f3d6719cd5cfbb9e417f564aea9b4ea7
birishh <birish.liku.w@gmail.com>
2026-05-14 17:33:24 +0300

    Update queries.sql
    Inserting data into the student table

--- mysql/queries.sql
+++ mysql/queries.sql
@@ -9,3 +9,10 @@ INSERT INTO Instructor VALUES
 (202, 'Hana', 'Tesfaye', 'hana@aastu.edu.et', 2),
 (203, 'Samuel', 'Taye', 'samuel@aastu.edu.et', 3),
 (204, 'Rahel', 'Kebede', 'rahel@aastu.edu.et', 4);
+
+INSERT INTO Student VALUES
+(101, 'Abel', 'Tadesse', 'abel@gmail.com', '0911223344', '2003-04-12', 1),
+(102, 'Meron', 'Ali', 'meron@gmail.com', '0911334455', '2002-08-21', 2),
+(103, 'Dawit', 'Kassa', 'dawit@gmail.com', '0911445566', '2001-07-10', 1),
+(104, 'Bethel', 'Mulu', 'bethel@gmail.com', '0911556677', '2004-01-17', 3),
+(105, 'Nahom', 'Teklu', 'nahom@gmail.com', '0911667788', '2002-05-25', 4);
